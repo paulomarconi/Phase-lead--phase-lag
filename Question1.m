0c89f889549bb58315c11e9b64565e41870d3fc6
Paulo Loma Marconi <zurits@gmail.com>
2020-06-07 00:27:41 -0400

    update

---
+++ Question1.m
@@ -0,0 +1,73 @@
+%% ========================================================================
+% ---- ACS6101 Assignment week 4
+% ---- Registration number: 180123717
+% ---- Name: Paulo Roberto Loma Marconi
+% ---- 23/10/2018
+%% === Question 1 =========================================================
+clear; clc; close all;
+%% plant G parameters
+s = tf('s');
+a = 2.5; b = 27;
+%% a) Gain K for PO=10
+PO = 10; % percentage overshoot
+zeta = log(100/PO)/sqrt(pi^2+ (log(100/PO))^2 ); % damping ratio
+PM_d = round(100*zeta)+1; % PM desired at the nearest round value
+h = tand(180-90-PM_d); 
+omega_c = roots([1 (a+b)/h -a*b]); % new omega_c
+K = omega_c(2)*sqrt(omega_c(2)^2+a^2)*sqrt(omega_c(2)^2+b^2); % new gain K
+G = K/( s*(s+a)*(s+b) ); % Plant G
+figure(1);
+margin(G);
+figure(2);
+step(feedback(G,1));
+%% b) Phase-lead compensator
+Kv = 25; % velocity error
+Kg = Kv*a*b; % loop gain to satisfy Kv
+G = Kg/( s*(s+a)*(s+b) ); % Plant G
+% omega_c for the uncompensated system
+omega_c = real( sqrt( roots([1 a^2+b^2 a^2*b^2 -Kg^2]) ) );
+% obtaining actual PM
+PM_act = 180-90-atand(omega_c(3)/a)-atand(omega_c(3)/b);
+% additional phase angle from the compensator PM
+theta = 0; % factor of correction
+PM_c = round(PM_d+theta-PM_act);
+% calculating alpha
+alpha = round( (sind(PM_c)+1)/(1-sind(PM_c)) );
+% Determine the new cross over frequency
+omega_c_new = real( sqrt( roots([1 a^2+b^2 a^2*b^2 -Kg^2*alpha]) ) );
+% Calculating the zero of the compensator
+z = omega_c_new(3)/sqrt(alpha);
+% Calculating the pole of the compensator
+p = alpha*z;
+% lead compensator
+Gc = (s+z)/(s+p);
+% new gain of the compensated system
+K_new = sqrt(alpha)*Kg;
+% compensated open-loop 
+Gol = K_new*Gc*G/Kg;
+% closed-loop 
+Gcl = feedback(Gol,1);
+%% c) evaluating the system to unit step and unit ramp
+figure(2);
+margin(Gol);
+figure(3);
+subplot(2,1,1);
+step = 1/s; % step input
+impulse(step,Gcl*step); 
+title('Step response');
+subplot(2,1,2);
+ramp = 1/s^2; % ramp input
+impulse(ramp,Gcl*ramp);
+title('Ramp response');
+% evaluating Kv for the new compensated system
+Kv_new = (z/p)*K_new/(a*b);
+% steady-state error to a unity ramp
+ess_ramp = 1/Kv_new;
+stepinfo(Gcl)
+% bandwidth of Gcl
+BW = bandwidth(Gcl);
+% resonant frequency
+omega_r = 4/(0.6*1.03) *sqrt(1-2*0.6^2);
+
+% syms s
+% ess = double(limit(s*Gol,s,0))
---
+++ Question2.m
@@ -0,0 +1,159 @@
+%% ========================================================================
+% ---- ACS6101 Assignment week 4
+% ---- Registration number: 180123717
+% ---- Name: Paulo Roberto Loma Marconi
+% ---- 27/10/2018
+%% === Question 2 =========================================================
+clear; clc; close all;
+%% plant G parameters
+s = tf('s');
+a = 9; b = 50;
+%% a) Location of the dominant poles for PO=20 and ts=2.9
+PO = 20; % percentage overshoot
+ts = 2.9; % settling time
+zeta = log(100/PO)/sqrt(pi^2+ (log(100/PO))^2 ); % damping ratio
+zeta = round(zeta,2)-0.01;
+omega_n = 4/(zeta*ts);
+% desired location of dominant poles
+s1 = -omega_n*zeta+omega_n*sqrt(1-zeta^2)*1i;
+%% c) Phase lead compensator
+z_lead = 1; % location of the desired zero
+% using the angle condition to determine the location of the pole
+x = -real(s1); y = imag(s1);
+h = 180 + 180 - atand(y/(x-z_lead))-2*( 180-atand(y/x) )-atand(y/(a-x))...
+    -atand(y/(b-x));
+p_lead = y/tand(h)+x;
+% Gc compensator TF
+Gc_lead = (s+z_lead)/(s+p_lead);
+% obtaining the gain K with the gain condition equation
+K = ((-x)^2+y^2)*sqrt((-x+a)^2+y^2)*sqrt((-x+b)^2+y^2)*...
+    sqrt((-x+p_lead)^2+y^2)/sqrt((-x+z_lead)^2+y^2);
+% the open-loop system
+G = K/(s^2*(s+a)*(s+b));% plant G
+Gol_lead = Gc_lead*G; % open-loop with the Lead compensator
+Gcl_lead = feedback(Gol_lead,1); % closed-loop with the Lead compensator
+
+fig = figure(1);
+rlocus(Gcl_lead);
+hold;
+% ploting the s1 and zeta in the rlocus
+n = 0:1:160; m = n*sqrt(zeta^2/(1-zeta^2));
+axis ([ -4 1 -4 4]);
+plot (-m,n,'--'); % zeta
+plot (-x,y,'rd');
+saveas(fig,'Q2_Lead_rlocus.png');
+
+fig = figure(2);
+step(Gcl_lead);
+saveas(fig,'Q2_Lead_step.png');
+
+fig = figure(3);
+margin(Gcl_lead);
+saveas(fig,'Q2_Lead_margin.png');
+BW_lead = bandwidth(Gcl_lead); % bandwidth
+
+%% Using prefilter to reduce the overshoot
+pf = z_lead; % selecting the zero of the lead compensator (z_lead)
+Gpf = pf/(s+pf);
+
+fig = figure(3);
+step(Gpf*Gcl_lead);
+saveas(fig,'Q2_Lead+prefilter_step.png');
+
+fig = figure(4);
+margin(Gpf*Gcl_lead);
+saveas(fig,'Q2_Lead+prefilter_margin.png');
+BW_lead_prefilter = bandwidth(Gpf*Gcl_lead); % bandwidth
+
+%% d) Phase-lag compensator
+ess_a = 0.025; % steady-state error for a parabolic input 0.5At^2
+Ka_d = 1/ess_a; % Ka desired
+Ka_act = (z_lead/p_lead)*(K/(a*b));
+% calculating the zero and pole of the compensator
+alpha = Ka_d/Ka_act;
+z_lag = abs(x)/10;
+p_lag = z_lag/alpha;
+% Gc phase-lag compensator
+Gc_lag = (s+z_lag)/(s+p_lag);
+%% Evaluating the performance with the phase-lead, phase-lag and prefilter
+Gol_lead_lag = Gc_lead*Gc_lag*G;
+Gcl_lead_lag = feedback(Gol_lead_lag,1);
+
+fig = figure(5); 
+rlocus(Gcl_lead_lag);
+hold;
+% ploting the s1 and zeta in the rlocus
+n = 0:1:160; m = n*sqrt(zeta^2/(1-zeta^2));
+axis ([ -4 1 -4 4]);
+plot (-m,n,'--'); % zeta
+plot (-x,y,'rd');
+saveas(fig,'Q2_Lead+Lag_rlocus.png');
+
+fig = figure(6);
+step(Gcl_lead,Gpf*Gcl_lead,Gcl_lead_lag,Gpf*Gcl_lead_lag)
+legend('Lead','Pre-filter + Lead','Lead + Lag','Pre-filter + Lead + Lag'...
+    ,'Location','southeast');
+saveas(fig,'Q2_All_step_1.png');
+stepinfo(Gpf*Gcl_lead_lag)
+
+fig = figure(7);
+margin(Gcl_lead_lag);
+saveas(fig,'Q2_Lead+lag_margin.png');
+BW_lead_lag = bandwidth(Gcl_lead_lag); % bandwidth
+
+fig = figure(8);
+margin(Gcl_lead_lag);
+saveas(fig,'Q2_Lead+lag+prefilter_margin.png');
+BW_lead_lag_prefilter = bandwidth(Gpf*Gcl_lead_lag); % bandwidth
+
+%% Verifing Ka = 40
+Ka = (z_lag/p_lag) * (z_lead/p_lead) * ( K/(a*b) );
+ess_parabolic = a/Ka; 
+
+%% New phase-lead compensator
+z_lead2 = x; % given zero lead right below the desired pole
+f = 180 + atand(y/(x-z_lead2))+(180-atand(y/(x-z_lag)))...
+    +(180-atand(y/(x-z_lead))) - (180-atand(y/(x-p_lag)))-atand(y/(p_lead-x))...
+    -2*(180-atand(y/x))-atand(y/(a-x))-atand(y/(b-x));
+% p_lead2 = y/tand(180+f) - x;
+p_lead2 = y/tand(f) + x;
+Gc_lead2 = (s+z_lead2)/(s+p_lead2);
+% Calculating the new gain K with the lead 2 compensator
+K_new = sqrt((-x+p_lead2)^2+y^2)*sqrt((-x+p_lag)^2+y^2)*...
+    sqrt((-x+p_lead)^2+y^2)*((-x)^2+y^2)*sqrt((-x+a)^2+y^2)*sqrt((-x+b)^2+y^2)/...
+    ( sqrt((-x+z_lead)^2+y^2)*sqrt((-x+z_lag)^2+y^2)*sqrt((-x+z_lead)^2+y^2)*K );
+Gol_lead2_lead_lag = K_new*Gc_lead2*Gol_lead_lag;
+Gcl_lead2_lead_lag = feedback(Gol_lead2_lead_lag,1);
+%% Verifing Ka = 40 
+Ka_new = K_new*(z_lead2/p_lead2)*(z_lag/p_lag)*(z_lead/p_lead)*(K/(a*b));
+ess_parabolic_new = a/Ka_new; 
+%% Comparing the new compensator of the previous designs.
+fig = figure(9);
+rlocus(Gcl_lead2_lead_lag)
+hold on;
+% ploting the s1 and zeta in the rlocus
+n = 0:1:160; m = n*sqrt(zeta^2/(1-zeta^2));
+axis ([ -4 1 -4 4]);
+plot (-m,n,'--'); % zeta
+plot (-x,y,'rd');
+saveas(fig,'Q2_Lead2+Lead+Lag_rlocus.png');
+
+fig = figure(10);
+% step(Gcl_lead2_lead_lag,Gpf*Gcl_lead2_lead_lag)
+step(Gcl_lead,Gpf*Gcl_lead,Gcl_lead_lag,Gpf*Gcl_lead_lag,...
+    Gcl_lead2_lead_lag,Gpf*Gcl_lead2_lead_lag)
+legend('Lead','Pre-filter+Lead','Lead+Lag','Pre-filter+Lead+Lag'...
+    ,'Lead 2+Lead+Lag','Pre-filter+Lead2+Lead+Lag','Location','southeast');
+saveas(fig,'Q2_All_step_2.png');
+stepinfo(Gpf*Gcl_lead2_lead_lag)
+
+fig = figure(11);
+margin(Gcl_lead2_lead_lag);
+saveas(fig,'Q2_Lead2+lead+lag_margin.png');
+BW_lead2_lead_lag = bandwidth(Gcl_lead2_lead_lag); % bandwidth
+
+fig = figure(12);
+margin(Gpf*Gcl_lead2_lead_lag);
+saveas(fig,'Q2_Lead2+lead+lag+prefilter_margin.png');
+BW_lead2_lead_lag_prefilter = bandwidth(Gpf*Gcl_lead2_lead_lag); % bandwidth
+
